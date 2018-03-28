880ac959f42d5565c7d8baf2e77407583e5c05a4
Eduardo Orive <edorka@gmail.com>
2018-03-28 16:28:56 +0200

    trying to create a pip package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='blockpy',
+    version='0.5.0',
+    url='https://github.com/Edorka/blockpy.git',
+    author='Eduardo Orive',
+    author_email='edorka@gmail.com',
+    description='Minimal implementation of a blockchain',
+    packages=find_packages(),
+    install_requires=[],
+)
