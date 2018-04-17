f37dc6612d21087828941923c443f12532b69219
Eduardo Orive <edorka@gmail.com>
2018-04-17 23:50:44 +0200

    added a test to verify the hash formation

--- tests/test_block.py
+++ tests/test_block.py
@@ -2,6 +2,8 @@ import unittest
 from unittest import mock
 from block import Block
 import datetime
+import hashlib
+import json
 
 
 class BlockTestCase(unittest.TestCase):
@@ -25,13 +27,30 @@ class BlockTestCase(unittest.TestCase):
 
     def test_timestamp_as_attribute(self):
         default_datetime = datetime.datetime(2010, 1, 1, 10, 20, 30)
-        selected_datetime = datetime.datetime(2018, 3, 1, 10, 20, 30)
+        selected_datetime = datetime.datetime(2018, 3, 1, 11, 21, 33)
         with mock.patch('datetime.datetime') as mock_datetime:
             mock_datetime.today.return_value = default_datetime
             block = Block(0, '000000000', {'message': 'do not panic'},
                           timestamp=selected_datetime.timestamp())
             expected = selected_datetime.timestamp()
             self.assertEqual(block.timestamp, expected)
+
+    @mock.patch.object(hashlib, 'sha256')
+    def test_obtained_hash(self, mocked):
+        selected_datetime = datetime.datetime(2018, 3, 1, 11, 21, 33)
+        expected = 'hashed'.encode('utf-8')
+        index = 0
+        expected = str(index)
+        previous_hash = '000000000'
+        expected += str(previous_hash)
+        selected_timestamp = selected_datetime.timestamp()
+        expected += str(selected_timestamp)
+        content = {'message': 'do not panic'}
+        expected += str(json.dumps(content))
+        Block(index, previous_hash, content, selected_timestamp)
+        args, kwargs = mocked.call_args
+        obtained = args[0]
+        self.assertIn(expected.encode(), obtained)
 
     def test_genesis_valid_next(self):
         data = {
